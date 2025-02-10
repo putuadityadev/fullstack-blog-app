12bf85f39e9e71ed20d0fd83bf73c028680c2ae2
aditya <adityamph1@gmail.com>
2025-02-10 21:39:38 +0800

    add env port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const multer = require("multer")
 const upload = multer()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
